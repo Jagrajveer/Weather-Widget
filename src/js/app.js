bfd8b639565bb75b1f48f8e515e6a9467200ca7c
jagrajveer <jagrajveer9@gmail.com>
2021-12-11 23:34:03 -0600

    add function to render current weather data

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,45 @@
+// API key for openweather api
 const API_TOKEN = "ff5ab50e290723493fcd206af6bc109a";
 
-async function getCurrentWeatherData(lat, lon) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_TOKEN}`;
+// Elements
+const CurrentConditionElement = document.querySelector(".current-conditions");
+const ForecastElement = document.querySelector(".forecast");
+
+// function to fetch current weather data from openweather api
+const getCurrentWeatherData = async (latitude, longitude) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_TOKEN}`;
   const data = await (await fetch(url)).json();
   return data;
-}
+};
 
-async function getFiveDayWeatherData(lat, lon) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_TOKEN}`;
+// function to fetch five day weather data from openweather api
+const getFiveDayWeatherData = async (latitude, longitude) => {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_TOKEN}`;
   const data = await (await fetch(url)).json();
   return data;
-}
+};
 
-function convertKelvinToCelcius(temp) {
+// function to convert temperature kelvin to celcius
+const convertKelvinToCelcius = (temp) => {
   return (temp - 273.15).toFixed(0);
-}
+};
 
+const renderCurrentWeatherData = (temperature, weather, icon) => {
+  CurrentConditionElement.innerHTML = `
+    <h2>Current Conditions</h2>
+    <img src="http://openweathermap.org/img/wn/${icon}@2x.png" />
+    <div class="current">
+    <div class="temp">${temperature}℃</div>
+    <div class="condition">${weather}</div>
+    </div>
+  `;
+};
+
+// Get coordinates of user's current location
 navigator.geolocation.getCurrentPosition(
   ({ coords: { latitude, longitude } }) => {
     getCurrentWeatherData(latitude, longitude)
-      .then((data) => {
-        console.log(convertKelvinToCelcius(data.main.temp));
-      })
+      .then((data) => console.log(data))
       .catch((error) => console.log(error));
 
     getFiveDayWeatherData(latitude, longitude);
