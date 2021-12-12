46b4800f8bbc1dc8b65ba32a256f9deec6007d63
jagrajveer <jagrajveer9@gmail.com>
2021-12-12 02:29:26 -0600

    add function to minify css and move it to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 const { src, dest, series } = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
 
 const moveHTML = () => {
   return src("src/*.html").pipe(dest("dist"));
 };
 
-exports.default = series(moveHTML);
+const minifyCSS = () => {
+  return src("src/css/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist/css"));
+};
+
+exports.default = series(moveHTML, minifyCSS, minifyJS);
 exports.html = moveHTML;
+exports.css = minifyCSS;
